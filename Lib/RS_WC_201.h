86d348b420cb324b46b7028f3d0b906e66cd0359
Patrick Servello <patrick.servello@gmail.com>
2015-01-30 13:23:27 +0000

    weird spacing issues.

--- Lib/RS_WC_201.h
+++ Lib/RS_WC_201.h
@@ -2,33 +2,33 @@
 #define RS_WC_201.h
 
 
-#define SEND_DATA			0x00
-#define SET_OP_MODE			0x10
-#define SET_BAND			0x11
-#define INIT				0x12
-#define SCAN				0x13
-#define JOIN				0x14
-#define SET_PWR_MODE		0x15
-#define SET_SLEEP_TMR   	0x16
-#define QUERY_NET_PARAM 	0x18
-#define DISCONNECT			0x19
-#define QUERY_RSSI			0x1A
-#define SELECT_ANTENNA		0x1B
-#define SOFT_RESET			0x1C
-#define FEATURE_SELECT 		0x1E
-#define QUERY_SNR			0x1F
-#define STORE_CONFIG		0X20
-#define ENABLE_AUTO_JOIN    0x21
-#define CREATE_AUTO_JOIN	0x21
-#define GET_CONFIG_INFO		0x22
-#define CONFIG_AP_MODE		0x24
-#define SET_WEP_KEY			0x25
-#define CONFIG_GPIO			0x28
-#define PING_REQUEST		0x29
-#define QUERY_STATS			0x38
-#define SET_IP_PARAM		0x41
-#define CREATE_SOCKET		0x42
-#define CLOSE_SOCKET		0x43
+#define SEND_DATA							0x00
+#define SET_OP_MODE						0x10
+#define SET_BAND							0x11
+#define INIT									0x12
+#define SCAN									0x13
+#define JOIN									0x14
+#define SET_PWR_MODE					0x15
+#define SET_SLEEP_TMR   			0x16
+#define QUERY_NET_PARAM 			0x18
+#define DISCONNECT						0x19
+#define QUERY_RSSI						0x1A
+#define SELECT_ANTENNA				0x1B
+#define SOFT_RESET						0x1C
+#define FEATURE_SELECT 				0x1E
+#define QUERY_SNR							0x1F
+#define STORE_CONFIG					0X20
+#define ENABLE_AUTO_JOIN    	0x21
+#define CREATE_AUTO_JOIN			0x21
+#define GET_CONFIG_INFO				0x22
+#define CONFIG_AP_MODE				0x24
+#define SET_WEP_KEY						0x25
+#define CONFIG_GPIO						0x28
+#define PING_REQUEST					0x29
+#define QUERY_STATS						0x38
+#define SET_IP_PARAM					0x41
+#define CREATE_SOCKET					0x42
+#define CLOSE_SOCKET					0x43
 
 #endif
 
