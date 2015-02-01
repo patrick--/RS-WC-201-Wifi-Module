df16e73e948fb2bc8a06ac9fe85ce989fa0dbcdf
Patrick Servello <patrick.servello@gmail.com>
2015-02-01 13:09:12 +0000

    Added rest of command codes.

--- Lib/RS_WC_201.h
+++ Lib/RS_WC_201.h
@@ -1,34 +1,48 @@
 #ifndef RS_WC_201.h
 #define RS_WC_201.h
 
-
-#define SEND_DATA							0x00
-#define SET_OP_MODE						0x10
-#define SET_BAND							0x11
-#define INIT									0x12
-#define SCAN									0x13
-#define JOIN									0x14
-#define SET_PWR_MODE					0x15
-#define SET_SLEEP_TMR   			0x16
-#define QUERY_NET_PARAM 			0x18
-#define DISCONNECT						0x19
-#define QUERY_RSSI						0x1A
-#define SELECT_ANTENNA				0x1B
-#define SOFT_RESET						0x1C
-#define FEATURE_SELECT 				0x1E
-#define QUERY_SNR							0x1F
-#define STORE_CONFIG					0X20
-#define ENABLE_AUTO_JOIN    	0x21
-#define CREATE_AUTO_JOIN			0x21
-#define GET_CONFIG_INFO				0x22
-#define CONFIG_AP_MODE				0x24
-#define SET_WEP_KEY						0x25
-#define CONFIG_GPIO						0x28
-#define PING_REQUEST					0x29
-#define QUERY_STATS						0x38
-#define SET_IP_PARAM					0x41
-#define CREATE_SOCKET					0x42
-#define CLOSE_SOCKET					0x43
+/* Commands Codes */
+#define SET_OP_MODE             0x10
+#define SET_BAND                0x11
+#define INIT                    0x12
+#define SCAN                    0x13
+#define JOIN                    0x14
+#define SET_PWR_MODE            0x15
+#define SET_SLEEP_TMR           0x16
+#define QUERY_NET_PARAM         0x18
+#define DISCONNECT              0x19
+#define QUERY_RSSI              0x1A
+#define SELECT_ANTENNA          0x1B
+#define SOFT_RESET              0x1C
+#define FEATURE_SELECT          0x1E
+#define QUERY_SNR               0x1F
+#define STORE_CONFIG            0X20
+#define ENABLE_AUTO_JOIN        0x21
+#define CREATE_AUTO_JOIN        0x21
+#define GET_CONFIG_INFO         0x22
+#define CONFIG_AP_MODE          0x24
+#define SET_WEP_KEY             0x25
+#define CONFIG_GPIO             0x28
+#define PING_REQUEST            0x29
+#define QUERY_STATS             0x38
+#define SET_IP_PARAM            0x41
+#define CREATE_SOCKET           0x42
+#define CLOSE_SOCKET            0x43
+#define DNS_RESOLUTION          0x44
+#define QUERY_LTCP_STATUS       0x46
+#define QUERY_WLAN_STATUS       0x48
+#define QUERY_FIRMWARE_VER      0x49
+#define GET_MAC_ADDRESS         0x4A
+#define CONFIG_P2P              0x4B
+#define CONFIG_EAP              0x4C
+#define SET_CERTIFICATE         0x4D
+#define QUERY_GO_PARAM          0x4E
+#define LOAD_WEB_FIELDS         0x50
+#define LOAD_WEBPAGE            0x50
+#define HTTP_GET                0x51
+#define HTTP_POST               0x52
+#define DNS_SERVER              0x55
+#define GET_SOCKET_INFO         0x57
 
 #endif
 
